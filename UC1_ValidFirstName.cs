9face933629ed88ac3641f84d5b500e3c85a9515
RISHABH <gguptarishabh82@gmail.com>
2022-04-04 23:08:12 +0530

    As a User need to enter a valid First Name

---
+++ UC1_ValidFirstName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_21_Registration_ProblemUsing_Exceptions
+{
+    internal class UC1_ValidFirstName
+    {
+        public void validateFirstName(string firstName)
+        {
+            string stringForFirstName = "^[A-Z][a-z]{3,}?";
+            try
+            {
+
+                if (Regex.IsMatch(firstName, stringForFirstName))
+                    Console.WriteLine(firstName + " is Valid");
+
+                else
+                    Console.WriteLine(firstName + " is Invalid");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
