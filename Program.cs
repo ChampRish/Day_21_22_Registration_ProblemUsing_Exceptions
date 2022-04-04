7af6b748a05c1a9481d77504c51a948f35d139c3
RISHABH <gguptarishabh82@gmail.com>
2022-04-04 23:05:58 +0530

    Calling All the UC

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_21_Registration_ProblemUsing_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_ValidFirstName firstname = new UC1_ValidFirstName();
+            firstname.validateFirstName("Rishabh");
+
+            UC2_ValidlastName lastName = new UC2_ValidlastName();
+            lastName.validateLastName("Gupta");
+
+            UC3_validateEmailId emailValid = new UC3_validateEmailId();
+            emailValid.validateEmailId("ggupta123@gmail.com");
+
+            UC4_validateMobileNumber phoneNumber = new UC4_validateMobileNumber();
+            phoneNumber.validateMobileNumber("9627974345");
+
+            UC5_validatePassword password = new UC5_validatePassword();
+            password.validatePassword("GGupta@1234");
+
+            UC6_validateUperCasePassword UpperCase = new UC6_validateUperCasePassword();
+            UpperCase.validateUperCasePassword("G12345678");
+
+            UC7_validateNumericPassword NumericPass = new UC7_validateNumericPassword();
+            NumericPass.validateNumericPassword("12345678");
+
+            UC8_Special_Character special = new UC8_Special_Character();
+            special.SpecialPassword("12Rish@fyjf");
+        }
+    }
+}
