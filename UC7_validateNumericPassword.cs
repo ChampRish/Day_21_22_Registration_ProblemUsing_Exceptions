cb58dfd38a719aaf5a4c702567b13ce8f22747ac
RISHABH <gguptarishabh82@gmail.com>
2022-04-04 23:20:31 +0530

    Rule3–Pass Should have at least 1 numeric number in the password

---
+++ UC7_validateNumericPassword.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_21_Registration_ProblemUsing_Exceptions
+{
+    internal class UC7_validateNumericPassword
+    {
+        public void validateNumericPassword(string NumericPassword)
+        {
+            try
+            {
+                string stringForNumericPassword = "^.{8,}?";
+                if (Regex.IsMatch(NumericPassword, stringForNumericPassword))
+                    Console.WriteLine(NumericPassword + " is Valid");
+                else
+                    Console.WriteLine(NumericPassword + " is Invalid");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
