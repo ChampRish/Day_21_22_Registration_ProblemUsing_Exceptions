6b3d585fca72d4d32b271a8ca55a7b7c942f015b
RISHABH <gguptarishabh82@gmail.com>
2022-04-04 23:12:21 +0530

    As a User need to enter a valid Last Name

---
+++ UC2_ValidlastName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_21_Registration_ProblemUsing_Exceptions
+{
+    internal class UC2_ValidlastName
+    {
+        public void validateLastName(string lastName)
+        {
+            try
+            {
+
+                string stringForLastName = "^[A-Z][a-z]{3,}?";
+                if (Regex.IsMatch(lastName, stringForLastName))
+                    Console.WriteLine(lastName + " is Valid Lastname");
+                else
+                    Console.WriteLine(lastName + " is Invalid Lastname");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
