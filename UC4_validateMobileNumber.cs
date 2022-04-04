dfde40613c2f098970b4dfab5c836661e4df205c
RISHABH <gguptarishabh82@gmail.com>
2022-04-04 23:15:12 +0530

    As a User need to follow pre-defined Mobile Format

---
+++ UC4_validateMobileNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_21_Registration_ProblemUsing_Exceptions
+{
+    internal class UC4_validateMobileNumber
+    {
+        public void validateMobileNumber(string mobileNumber)
+        {
+            try
+            {
+
+                string stringForMobileNumber = "^[0-9]{10}";
+                if (Regex.IsMatch(mobileNumber, stringForMobileNumber))
+                    Console.WriteLine(mobileNumber + " is Valid MobileNumber");
+                else
+                    Console.WriteLine(mobileNumber + " is Invalid MobileNumber");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
