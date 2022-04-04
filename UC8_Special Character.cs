3953df44b13a5e8f3611e1ad500bbfaa0848182f
RISHABH <gguptarishabh82@gmail.com>
2022-04-04 23:22:08 +0530

    Rule4–Password Has exactly 1 Special Character

---
+++ UC8_Special Character.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_21_Registration_ProblemUsing_Exceptions
+{
+    internal class UC8_Special_Character
+    {
+        public void SpecialPassword(string specialvalidate)
+        {
+            try
+            {
+                string RegexPattern = "^[a-zA-Z0-9]{1,}[@_?&*;:!$]{1}[a-zA-Z0-9]{1,}$";
+                
+                if (Regex.IsMatch(specialvalidate, RegexPattern))
+                    Console.WriteLine(specialvalidate + " is Valid");
+                else
+                    Console.WriteLine(specialvalidate + " is Invalid");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        
+        
+    }
+}
